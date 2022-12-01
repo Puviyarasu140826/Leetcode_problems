3057654eaeffb1066bcccf03a1a166045d658125
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-01 20:21:08 +0530

    Update Two Sum.cpp

--- Array/Two Sum/Two Sum.cpp
+++ Array/Two Sum/Two Sum.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& v, int target) {
+        unordered_map<int,int> m;
+        vector<int> r;
+        for(int i=0;i<v.size();i++)
+        {
+            int k=target-v[i];
+            if(m.count(k))
+            {
+                r={m[k],i};
+                return r;
+            }
+            m[v[i]]=i;
+        }
+        return {0};
 
+        
+    }
+};
