5d40ada39a1b9347c45f2847f0781191b3436901
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-04 22:24:53 +0530

    Update Remove Element.cpp

--- Array/Remove Element/Remove Element.cpp
+++ Array/Remove Element/Remove Element.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int removeElement(vector<int>& v, int val) {
+        int i=0,j=0;
+        while(j<v.size())
+        {
+            if(v[j]!=val)
+            {
+                v[i]=v[j];
+                i++;
+                j++;
+            }
+            else
+            j++;
+        }
+        return i;
+            
+    }
+};
