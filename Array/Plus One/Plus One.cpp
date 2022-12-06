f831dc7ee021ab4a04bd922459a27625eb6ef4cd
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-06 23:38:28 +0530

    Update Plus One.cpp

--- Array/Plus One/Plus One.cpp
+++ Array/Plus One/Plus One.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(),digits.end());
+        int one=1;
+        int i=0;
+        while(one)
+        {
+            if(i<digits.size())
+            {
+                if(digits[i]==9)
+                {
+                    digits[i]=0;
+                }
+                else
+                {
+                    digits[i]+=1;
+                    one=0;
+                }
+            }
+            else
+            {
+                digits.push_back(1);
+                one =0;
+            }
+            i++;
+        }
+        reverse(digits.begin(),digits.end());
+        return digits;
+        
+    }
+};
