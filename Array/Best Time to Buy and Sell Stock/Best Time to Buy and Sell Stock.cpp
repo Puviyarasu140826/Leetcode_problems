1d46fc5b3bd8b33ead551d34b4594a4363975fe6
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-09 22:22:38 +0530

    Update Best Time to Buy and Sell Stock.cpp

--- Array/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.cpp
+++ Array/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& v) {
+        int l=0,r=1;
+        int p=0,maxp=0;
+        while(r<v.size())
+        {
+            if(v[l]<v[r])
+            {
+            p=v[r]-v[l];
+            maxp=max(maxp,p);
+            }
+            else
+            {
+                l=r;
+            }
+            r++;
 
+        }
+        return maxp;
+    }
+};
