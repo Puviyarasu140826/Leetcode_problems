142eb9adc79ea6d85b38e0e95e4ed5a6e3693ef3
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-11 20:47:21 +0530

    Update Single Number.cpp

--- Array/Single Number/Single Number.cpp
+++ Array/Single Number/Single Number.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int m=nums.size();
+        int i=0;
+        for(i=0;i<m-1;i=i+2)
+        {
+            if(nums[i]==nums[i+1])
+                continue;
+            else
+                return nums[i];
+        } 
+        return nums[i];
+    }
+};
