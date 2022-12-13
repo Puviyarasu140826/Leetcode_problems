9806d207f867d2bd0cf7ac9dd3e5ba0cc16b30d8
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-13 21:26:27 +0530

    Update Majority Element.cpp

--- Array/Majority Element/Majority Element.cpp
+++ Array/Majority Element/Majority Element.cpp
@@ -1,1 +1,16 @@
-
+int majorityElement(int* v, int n){
+        int cnt=1;
+        int t=v[0];
+        for(int i=1;i<n;i++)
+        {
+            if(t==v[i])cnt++;
+            else cnt--;
+            if(cnt==0)
+            {
+                t=v[i];
+                cnt=1;
+            }
+            
+        }
+        return t;
+}
