3affde17a86efa0d85035d3e8d495f321d6cc5d1
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-15 19:43:39 +0530

    Update Contains Duplicate.cpp

--- Array/Contains Duplicate/Contains Duplicate.cpp
+++ Array/Contains Duplicate/Contains Duplicate.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& v) {
+        unordered_map<int,int> m;
+        for(auto i:v)
+            m[i]=m[i]+1;
+        for(auto i:m)
+        {
+            if(i.second>1)
+            return true;
+        }
+        return false;
+    }
+};
