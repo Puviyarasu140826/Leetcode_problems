021c7970d30547fe44714307b7508774b4e22bd2
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-17 20:07:05 +0530

    Update Missing Number.cpp

--- Array/Missing Number/Missing Number.cpp
+++ Array/Missing Number/Missing Number.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(auto i:nums)
+        m[i]=1;
+        for(int i=0;i<=nums.size();i++)
+        {
+            {
+                if(m[i]==0)
+                return i;
+            }
+        }
+        return 0;
+    }
+};
