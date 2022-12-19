8ce058e613d9b91a985b679908d6defe58c23e26
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-19 20:16:51 +0530

    Update Move Zeroes.cpp

--- Array/Move Zeroes/Move Zeroes.cpp
+++ Array/Move Zeroes/Move Zeroes.cpp
@@ -1,1 +1,14 @@
-
+void moveZeroes(int* a, int numsSize){
+    int cnt=0;
+    int t=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(a[i])
+        {
+            t=a[i];
+            a[i]=a[cnt];
+            a[cnt]=t;
+            cnt++;
+        }
+    }
+}
