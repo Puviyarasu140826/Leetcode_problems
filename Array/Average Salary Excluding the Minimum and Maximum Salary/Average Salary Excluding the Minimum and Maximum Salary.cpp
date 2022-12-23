f6467cdcbb46273fffeebabc134e9f2b4d625bd0
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-23 21:20:14 +0530

    Update Average Salary Excluding the Minimum and Maximum Salary.cpp

--- Array/Average Salary Excluding the Minimum and Maximum Salary/Average Salary Excluding the Minimum and Maximum Salary.cpp
+++ Array/Average Salary Excluding the Minimum and Maximum Salary/Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    double average(vector<int>& v) {
+        int n=v.size();
+        double sum,min,max,avg=0.0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=v[i];
+        }
+        min=*min_element(v.begin(),v.end());
+        max=*max_element(v.begin(),v.end());
+        avg=(sum-(min+max))/(n-2);
+        return avg;
+    }
+};
