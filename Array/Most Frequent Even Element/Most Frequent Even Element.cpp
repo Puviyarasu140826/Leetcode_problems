235dd0a66b476ff39b9071f077d652f3dbf5893e
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-25 22:34:36 +0530

    Update Most Frequent Even Element.cpp

--- Array/Most Frequent Even Element/Most Frequent Even Element.cpp
+++ Array/Most Frequent Even Element/Most Frequent Even Element.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& v) {
+        map<int,int> m;
+        for(auto i: v)
+        {
+            if(i%2==0)
+            {
+                m[i]=m[i]+1;
+            }
+        }
+        int f=0,ans=-1;
+        for(auto i: m)
+        {
+            if(i.second>f)
+            {
+                ans=i.first,f=i.second;
+            }
+        }
+        return ans;
+        
+    }
+};
