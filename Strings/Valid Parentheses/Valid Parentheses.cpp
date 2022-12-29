01e8eabc4d9b65f9a81dac1ef6cb009c31f967fc
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-12-29 20:50:12 +0530

    Update Valid Parentheses.cpp

--- Strings/Valid Parentheses/Valid Parentheses.cpp
+++ Strings/Valid Parentheses/Valid Parentheses.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> m;
+        int i=0;
+        while(s[i]!='\0')
+        {
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+            m.push(s[i]);
+            if(s[i]==')'||s[i]==']'||s[i]=='}')
+            {
+                if(m.empty()) return false;
+                if(s[i]==')'&&m.top()=='('||s[i]==']'&&m.top()=='['||s[i]=='}'&&m.top()=='{')
+                m.pop();
+                else
+                return false;
+            }
+            i++;
+        }
+        if(m.empty())
+        return true;
+        return false;
+    }
+};
