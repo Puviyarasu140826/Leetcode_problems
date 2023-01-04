c7cd9d931d96722773f6d0feaec1bac73ddc1f2c
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-01-04 22:55:03 +0530

    Update Rotate String.cpp

--- Strings/Rotate String/Rotate String.cpp
+++ Strings/Rotate String/Rotate String.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.length();
+        for(auto i:s)
+        {
+            reverse(s.begin(),s.end());
+            reverse(s.begin()+1,s.end());
+            if(s==goal)
+            return true;
+        }
+        return false;
+    }
+};
