95a8ad7d7b64661d331358d99dc383b89a8acd33
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-01-07 21:07:15 +0530

    Update Find the Difference.cpp

--- Strings/Find the Difference/Find the Difference.cpp
+++ Strings/Find the Difference/Find the Difference.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int>count(128);
+        for(const auto i:s)
+        ++count[i];
+        for(const auto i:t)
+        if(--count[i]<0)
+        return i;
+        throw;
+    }
+};
