e2d58fe7ec9b9b892bf3ad47e15796eb5ff6710d
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-01-15 21:38:01 +0530

    Update Add Binary.cpp

--- Strings/Add Binary/Add Binary.cpp
+++ Strings/Add Binary/Add Binary.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i=a.length()-1;
+        int j=b.length()-1;
 
+        string ans="";
+        int carry,aval,bval;
+        carry=0;
+
+        while(i>=0 || j>=0 || carry>0)
+        {
+            aval=0;
+            bval=0;
+            if(i>=0)
+            {
+                aval=a[i]-'0';
+            }
+            if(j>=0)
+            {
+                bval=b[j]-'0';
+            }
+
+            int sum=aval+bval+carry;
+            char c=sum%2+'0';
+            ans=c+ans;
+            carry=0;
+            if(sum>1)
+            {
+                carry=1;
+            }
+            i--;
+            j--;
+        }
+        return ans;
+    }
+};
