e614deae412a0dfa53a4c3567aa9033271ec59c8
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-01-16 21:29:37 +0530

    Update Valid Palindrome.cpp

--- Strings/Valid Palindrome/Valid Palindrome.cpp
+++ Strings/Valid Palindrome/Valid Palindrome.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+        for(auto i : s) {
+            if(isupper(i)) {
+                i = tolower(i);
+            }
+            if(isalnum(i)) {
+                str.push_back(i);
+            }
+        }
+        string rev = str;
+        reverse(rev.begin(), rev.end());
+        return str == rev;
+    }
+};
