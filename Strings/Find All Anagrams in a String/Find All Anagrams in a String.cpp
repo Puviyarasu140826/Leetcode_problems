6de33a620433bd48a614e7c204f42da56f361934
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-01-28 21:36:48 +0530

    Update Find All Anagrams in a String.cpp

--- Strings/Find All Anagrams in a String/Find All Anagrams in a String.cpp
+++ Strings/Find All Anagrams in a String/Find All Anagrams in a String.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+         vector<int>result;
+        vector<int>v1(26);
+        if(s.size()<p.size()){
+            return result;
+        }
+        for(int i=0;i<p.size();i++){
+            v1[p[i]-97]++;
+        }
+        for(int i=0;i<=(s.size()-p.size());i++){
+            vector<int>v2(26);
+            for(int index=i;index<(i+p.size());index++){
+                v2[s[index]-97]++;
+            }
+            if(v1==v2){
+                result.push_back(i);
+            }        
+        }
+        return result;
+        
+    }
+};
