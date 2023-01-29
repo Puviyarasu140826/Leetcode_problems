5746d94ec962bc94e4356fcea052e22756b8c038
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-01-29 20:20:25 +0530

    Update Check if the Sentence Is Pangram.cpp

--- Strings/Check if the Sentence Is Pangram/Check if the Sentence Is Pangram.cpp
+++ Strings/Check if the Sentence Is Pangram/Check if the Sentence Is Pangram.cpp
@@ -1,1 +1,17 @@
-
+bool checkIfPangram(char * s){
+    int arr[26]={0};
+    int i=0;
+    while(s[i]!='\0'){
+        if(s[i]>='a'&&s[i]<='z'){
+            arr[s[i]-'a']=1;
+        }
+        else if(s[i]>='A'&&s[i]<='B'){
+            arr[s[i]-'A']=1;
+        }
+        i++;
+    }
+    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+        if(arr[i]==0)return false;
+    }
+    return true;
+}
