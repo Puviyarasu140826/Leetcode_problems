c346253f5b20e855ff9277508995bb872101e779
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-01-30 22:07:19 +0530

    Update First Missing Positive.cpp

--- Array/First Missing Positive/First Missing Positive.cpp
+++ Array/First Missing Positive/First Missing Positive.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& arr) {
+        int n=arr.size();
+          for(int i=0;i<n;i++){
+        while(arr[i]>0 && arr[i]<=n && arr[i]!=arr[arr[i]-1])
+            swap(arr[i],arr[arr[i]-1]);
+    }
+        for(int i=0;i<n;i++){
+            if(arr[i]!=i+1) 
+            return i+1;
+        
+    }
+    return n+1;
+    }
+};
