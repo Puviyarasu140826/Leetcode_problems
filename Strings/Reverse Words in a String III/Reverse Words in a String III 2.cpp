14ddf4b084918ecd062b51c1c3b18c948e8609bd
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-01-31 22:05:55 +0530

    Update Reverse Words in a String III 2.cpp

--- Strings/Reverse Words in a String III/Reverse Words in a String III 2.cpp
+++ Strings/Reverse Words in a String III/Reverse Words in a String III 2.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string str="",tmp="";
+        int n=s.length();
+        for(int i=0;i<=n;i++){
+            if(i==n){
+                reverse(tmp.begin(),tmp.end());
+                str+=tmp;
+            }
+            else if(s[i]==' '||i==n){
+                reverse(tmp.begin(),tmp.end());
+                str+=tmp+" ";
+                tmp="";
+            }
+            else
+            tmp+=s[i];
 
+        }  
+        return str;      
+    }
+};
