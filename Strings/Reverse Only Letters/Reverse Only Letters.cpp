7460c284268e749f9ade230e33e4c5019f4ae475
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-01 21:30:20 +0530

    Update Reverse Only Letters.cpp

--- Strings/Reverse Only Letters/Reverse Only Letters.cpp
+++ Strings/Reverse Only Letters/Reverse Only Letters.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+   for(int i=0,j=s.length()-1;i<j;){
+       if(!isalpha(s[i]))
+       i++;
+       else if(!isalpha(s[j]))
+       j--;
+       else
+       swap(s[i++],s[j--]);
+   }
+   return s;
+    }
+};
