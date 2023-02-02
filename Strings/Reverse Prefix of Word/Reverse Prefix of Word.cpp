cb8a658c9e2816c90130bb0229e83839ffdfae7a
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-02 21:03:38 +0530

    Update Reverse Prefix of Word.cpp

--- Strings/Reverse Prefix of Word/Reverse Prefix of Word.cpp
+++ Strings/Reverse Prefix of Word/Reverse Prefix of Word.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    string reversePrefix(string s, char ch) {
+        string tmp=" ";
+        for(int i=0;i<s.length();i++){
+            //tmp+=s[i];
+            if(s[i]==ch){
+                reverse(begin(s),begin(s)+s.find(ch)+1);
+            }
+        }
+        
+        return s;
+    }
+};
