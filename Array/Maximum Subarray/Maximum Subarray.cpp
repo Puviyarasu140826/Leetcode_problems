0d538663d484ce5fa55e6bccf4d9828c239a0bee
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-03 18:42:49 +0530

    Update Maximum Subarray.cpp

--- Array/Maximum Subarray/Maximum Subarray.cpp
+++ Array/Maximum Subarray/Maximum Subarray.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curs=0,max_sum=INT_MIN;
+        for(auto x:nums){
+            if(curs<0) curs=0;
+            curs+=x;
+            max_sum=max(max_sum,curs);
+        }
+        return max_sum;
+        
+    }
+};
 
