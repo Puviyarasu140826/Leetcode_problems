e0293ff30f9b59f1e339d676dae9d76a2272069c
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-06 22:21:02 +0530

    Update Maximum Product Subarray.cpp

--- Array/Maximum Product Subarray/Maximum Product Subarray.cpp
+++ Array/Maximum Product Subarray/Maximum Product Subarray.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+         int max_ending_here = nums[0];
+        int min_ending_here = nums[0];
+        int max_so_far = nums[0];
+        int temp_maximum;
 
+        for(int i = 1; i < nums.size(); i++) {
+            temp_maximum = max({nums[i], nums[i] * max_ending_here, nums[i] * min_ending_here});
+            min_ending_here = min({nums[i], nums[i] * max_ending_here, nums[i] * min_ending_here});
+            max_ending_here = temp_maximum;
+            max_so_far = max(max_so_far, max_ending_here);
+        }
+
+        return max_so_far;
+        
+    }
+};
