ae797cce9dbaaa81b3d04621227a0c0a73d2440c
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-16 21:02:31 +0530

    Update Count Number of Distinct Integers After Reverse Operations.cpp

--- Array/Count Number of Distinct Integers After Reverse Operations/Count Number of Distinct Integers After Reverse Operations.cpp
+++ Array/Count Number of Distinct Integers After Reverse Operations/Count Number of Distinct Integers After Reverse Operations.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    int countDistinctIntegers(vector<int>& nums) {
+        unordered_set<int>numsset{begin(nums),end(nums)};
+        for(auto i:nums){
+            numsset.insert(reverse(i));
+        }
+        return numsset.size();
+        }
+        int reverse(int num){
+            int ans=0;
+            while(num>0){
+                ans=ans*10+num%10;
+                num=num/10;
+            }
+            return ans;
+        }
+};
