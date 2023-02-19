fb2186512cbf0b42c6e918de2043ee8c39b389c0
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-19 21:41:00 +0530

    Update Count Common Words With One Occurrence.cpp

--- Strings/Count Common Words With One Occurrence/Count Common Words With One Occurrence.cpp
+++ Strings/Count Common Words With One Occurrence/Count Common Words With One Occurrence.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        unordered_map<string,int>mp;
+        for(auto x:words1){
+            if(mp[x]==0) mp[x]=1;
+            else if(mp[x]==1) mp[x]=-1;
+        }
+        for(auto x:words2){
+            if(mp[x]==1) mp[x]=2;
+            else if(mp[x]==2) mp[x]=-1;
+        }
+        int count=0;
+        for(auto i:mp){
+            if(i.second==2)
+            count++;
+        }
+        return count;
+        
+    }
+};
