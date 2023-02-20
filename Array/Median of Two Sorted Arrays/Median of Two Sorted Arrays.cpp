51c8c1ccf90a4fcdb2c179154291f0be18290a9b
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-20 18:00:42 +0530

    Update Median of Two Sorted Arrays.cpp

--- Array/Median of Two Sorted Arrays/Median of Two Sorted Arrays.cpp
+++ Array/Median of Two Sorted Arrays/Median of Two Sorted Arrays.cpp
@@ -1,1 +1,26 @@
 
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        double ans;
+        for(auto i : nums2)
+        {
+            nums1.push_back(i);
+        }
+        sort(nums1.begin(),nums1.end());
+        if(nums1.size()==1)
+        return nums1[0];
+        if(nums1.size()==2)
+        return (nums1[0]+nums1[1])/2.0;
+        int n=nums1.size();
+        if(n%2==1)
+        {
+            ans=nums1[n/2];
+            return ans;
+        }
+    
+            ans=(nums1[n/2]+nums1[n/2-1])/2.0;
+            return ans;
+    
+    }
+};
