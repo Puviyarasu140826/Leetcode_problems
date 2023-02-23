9284c3e7d81258eea9b3bc4a0a5a8936f44d0c2f
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-23 21:19:59 +0530

    Update 4Sum.cpp

--- Array/4Sum/4Sum.cpp
+++ Array/4Sum/4Sum.cpp
@@ -1,1 +1,34 @@
 
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        long long target1=target;
+        sort(nums.begin(),nums.end());
+        set<vector<int>>s;
+        vector<vector<int>>output;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+            int c=j+1;
+            int k=nums.size()-1;
+            while(c<k){
+                long long int sum=nums[i];
+                sum+=nums[j];
+                sum+=nums[c];
+                sum+=nums[k];
+                if(sum==target1){
+                    s.insert({nums[i],nums[j],nums[c],nums[k]});
+                    c++;
+                    k--;
+                }
+                    else if(sum<target1){
+                        c++;
+                    }
+                    else k--;
+            }
+            }
+        }
+        for(auto i:s)
+        output.push_back(i);
+        return output;
+    }
+};
