d3f97ce251c8b4fb8bfd0096e333210fa6d0ef6c
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-24 19:23:48 +0530

    Update Search in Rotated Sorted Array.cpp

--- Array/Search in Rotated Sorted Array/Search in Rotated Sorted Array.cpp
+++ Array/Search in Rotated Sorted Array/Search in Rotated Sorted Array.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    int search(vector<int>& nums, int k) {
+        int l=0,h=nums.size()-1;
+        while(l<h){
+        int mid=(l+h)/2;
+        if(nums[mid]==k) return mid;
+        if(nums[l]<=nums[mid]){
+            if(k>=nums[l]&&k<nums[mid]) h=mid-1;
+            else l=mid+1;
+        }
+        else {
+            if(k<=nums[h]&&k>nums[mid]) l=mid+1;
+            else h=mid-1;
+        }
+        
+        }
+        if(nums[l]==k) return l;
+        else return -1;
+        
+    }
+};
