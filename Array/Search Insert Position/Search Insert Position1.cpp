4b3d36dcd310846e02c1441bbc4c52eff77521a4
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-25 20:17:56 +0530

    Update Search Insert Position1.cpp

--- Array/Search Insert Position/Search Insert Position1.cpp
+++ Array/Search Insert Position/Search Insert Position1.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    int searchInsert(vector<int>& v, int t) {
+        int i=0;
+        while(i<v.size())
+        {
+            if(v[i]==t)
+                return i;
+            i++;
+        }
+        if(t<v[0])
+            return 0;
+        else if(t>v[v.size()-1])
+            return v.size();
+        i=0;
+        while(i<v.size()-1)
+        {
+           if(t>v[i]&&t<v[i+1])
+           return i+1;
+           i++;
+        }
+    return i+1;
+    }
+};
