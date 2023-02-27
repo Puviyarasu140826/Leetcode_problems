7aef1bef7c8232437f67fcc6afb83ed30d54604e
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-27 21:31:30 +0530

    Update Jump Game II.cpp

--- Array/Jump Game II/Jump Game II.cpp
+++ Array/Jump Game II/Jump Game II.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int fast=0,jump=0,cur=0;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            fast=max(fast,nums[i]+i);
+            if(i==cur){
+                jump++;
+                cur=fast;
+            }
+        }
+       // if(cur>=nums.size()-1)
+        return jump;
+        //else return -1;
+    }
+};
