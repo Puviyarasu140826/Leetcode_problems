a0bafc34280c96d85cb6706aa10c29b8feac97f7
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-02-28 19:59:05 +0530

    Update Jump Game.cpp

--- Array/Jump Game/Jump Game.cpp
+++ Array/Jump Game/Jump Game.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int target=0;
+        for(int i=0;i<=target;i++){
+            target=max(target,nums[i]+i);
+            if(target>=nums.size()-1)
+            return true;
+        }
+        return false;
+    }
+};
