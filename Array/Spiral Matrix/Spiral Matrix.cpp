44c42abdc3858d4dfb1497c68266eab622683a79
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-03 20:34:09 +0530

    Update Spiral Matrix.cpp

--- Array/Spiral Matrix/Spiral Matrix.cpp
+++ Array/Spiral Matrix/Spiral Matrix.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int R=matrix.size(),c=matrix[0].size();
+        int t=0,l=0,b=R-1,r=c-1;
+        while(t<=b && l<=r){
+        for(int i=l;i<=r;i++)
+        ans.push_back(matrix[t][i]);
+        t++;
+        for(int j=t;j<=b;j++)
+        ans.push_back(matrix[j][r]);
+        r--;
+        if(t<=b){
+            for(int m=r;m>=l;m--)
+            ans.push_back(matrix[b][m]);
+            b--;
+        }
+        if(l<=r){
+        for(int n=b;n>=t;n--)
+        ans.push_back(matrix[n][l]);
+        l++;
+        }
+        }
+       return ans; 
+    }
+};
