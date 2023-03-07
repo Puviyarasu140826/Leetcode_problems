31b89f9ca0c84157310a128fba514fa326a79713
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-07 21:26:17 +0530

    Update Count Triplets That Can Form Two Arrays of Equal XOR.cpp

--- Array/Count Triplets That Can Form Two Arrays of Equal XOR/Count Triplets That Can Form Two Arrays of Equal XOR.cpp
+++ Array/Count Triplets That Can Form Two Arrays of Equal XOR/Count Triplets That Can Form Two Arrays of Equal XOR.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int sum=0,count=0;
+        for(int i=0;i<arr.size()-1;i++){
+            int x=arr[i];
+            for(int j=i+1;j<arr.size();j++){
+                x^=arr[j];
+                if(x==0)
+                count+=(j-i);
+            }
+        }
+       return count; 
+    }
+};
