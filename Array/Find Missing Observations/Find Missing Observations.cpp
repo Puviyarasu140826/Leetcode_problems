f26b03ff722ee79489d866af5a13a41cd068831e
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-08 22:21:48 +0530

    Update Find Missing Observations.cpp

--- Array/Find Missing Observations/Find Missing Observations.cpp
+++ Array/Find Missing Observations/Find Missing Observations.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+    int total =0,m=rolls.size();
+    for(auto i: rolls)
+    total+=i;
+    int missing_sum=mean*(n+m)-total;
+    if(missing_sum<n || missing_sum>6*n)
+    return{};
+    int quo=missing_sum/n,rem=missing_sum%n;
+    vector<int>ans(n,quo);
+    for(int i=0;i<rem;i++){
+        ++ans[i];
+    }
+      return ans;  
+    }
+};
