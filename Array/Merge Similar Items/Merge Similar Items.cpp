421cfdef6e40fcb62c2119f3a2d05c52741658a9
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-12 20:46:30 +0530

    Update Merge Similar Items.cpp

--- Array/Merge Similar Items/Merge Similar Items.cpp
+++ Array/Merge Similar Items/Merge Similar Items.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& item1, vector<vector<int>>& item2) {
+        map<int,int>mp;
+        for(auto x:item1) mp[x[0]]+=x[1];
+        for(auto x:item2) mp[x[0]]+=x[1];
+        vector<vector<int>>ans;
+        for(auto i:mp){
+            ans.push_back({i.first,i.second});
+        }
+        return ans;
+    }
+};
