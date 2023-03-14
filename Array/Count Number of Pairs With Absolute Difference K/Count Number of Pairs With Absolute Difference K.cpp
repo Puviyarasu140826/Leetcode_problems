4b0afb33e689af83fbd6d708af6e4730b57e5807
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-14 21:57:32 +0530

    Update Count Number of Pairs With Absolute Difference K.cpp

--- Array/Count Number of Pairs With Absolute Difference K/Count Number of Pairs With Absolute Difference K.cpp
+++ Array/Count Number of Pairs With Absolute Difference K/Count Number of Pairs With Absolute Difference K.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int ans=0,count=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                ans=abs(nums[i]-nums[j]);
+                if(ans==k) count++;
+            }
+        }
+       return count; 
+    }
+};
