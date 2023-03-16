a127fc9d0c7876ead6e964ca6f81c88c7427cf38
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-16 22:16:29 +0530

    Update Number of Pairs of Interchangeable Rectangles.cpp

--- Array/Number of Pairs of Interchangeable Rectangles/Number of Pairs of Interchangeable Rectangles.cpp
+++ Array/Number of Pairs of Interchangeable Rectangles/Number of Pairs of Interchangeable Rectangles.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& nums) {
+        long long result=0;
+        double ratio;
+        unordered_map<double,double>mp;
+        for(int i=nums.size()-1;i>=0;i--){
+            ratio=(double)nums[i][0]/(double)nums[i][1];
+            if(mp.find(ratio)!=end(mp))
+                result+=mp[ratio];
+                mp[ratio]++;
+            
+        }
+        return result;
+    }
+};
