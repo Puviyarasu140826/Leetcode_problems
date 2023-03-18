74ac5e08d59d5594eba591995922c0b778f7b01c
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-18 21:27:48 +0530

    Update Global and Local Inversions.cpp

--- Array/Global and Local Inversions/Global and Local Inversions.cpp
+++ Array/Global and Local Inversions/Global and Local Inversions.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    bool isIdealPermutation(vector<int>& nums) {
+        int count=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==i||nums[i]==i+1||nums[i]==i-1)
+            count++;
+        }
+        if(count==n) return true;
+        else 
+        return false;
+        
+    }
+};
