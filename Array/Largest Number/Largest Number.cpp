b46e83424d28b85c1d2538c11dfbae08202e062e
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-19 20:19:53 +0530

    Update Largest Number.cpp

--- Array/Largest Number/Largest Number.cpp
+++ Array/Largest Number/Largest Number.cpp
@@ -1,1 +1,16 @@
-
+bool comp(string a,string b){
+    return a+b>b+a;
+}
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string>ans;
+        for(auto i:nums)
+        ans.push_back(to_string(i));
+        sort(ans.begin(),ans.end(),comp);
+        string res;
+        for(auto i:ans)
+        res+=i;
+        return res[0]=='0'?"0":res;    
+    }
+};
