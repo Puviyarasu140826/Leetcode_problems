28c14142f7bbd6bd6fae99ace06ba3cd9b48a675
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-22 21:07:32 +0530

    Update Power of Two.cpp

--- Array/Power of Two/Power of Two.cpp
+++ Array/Power of Two/Power of Two.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        long long p=1;
+        int i=0;
+        while(p<=n){
+            if(p==n) 
+            return true;
+            i++;
+            p=pow(2,i);
+        }
+        return false;
+    }
+};
