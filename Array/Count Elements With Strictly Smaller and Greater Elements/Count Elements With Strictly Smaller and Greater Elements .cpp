2113b06717967496036934fb02e9a4bd3f544dac
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-25 19:26:59 +0530

    Update Count Elements With Strictly Smaller and Greater Elements .cpp

--- Array/Count Elements With Strictly Smaller and Greater Elements/Count Elements With Strictly Smaller and Greater Elements .cpp
+++ Array/Count Elements With Strictly Smaller and Greater Elements/Count Elements With Strictly Smaller and Greater Elements .cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+        int maxi=INT_MIN;
+        int mini=INT_MAX;
+        map<int,int>mp;
+        for(auto i:nums){
+            mp[i]++;
+            maxi=max(maxi,i);
+            mini=min(mini,i);
+        }
+        if(maxi==mini)return 0;
+        return nums.size()-(mp[maxi]+mp[mini]);
+        
+    }
+};
