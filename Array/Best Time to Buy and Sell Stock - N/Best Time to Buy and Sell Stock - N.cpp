576823986cb4c967936656cf2d6b0a49fc8b20c2
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2023-03-28 21:11:12 +0530

    Update Best Time to Buy and Sell Stock - N.cpp

--- Array/Best Time to Buy and Sell Stock - N/Best Time to Buy and Sell Stock - N.cpp
+++ Array/Best Time to Buy and Sell Stock - N/Best Time to Buy and Sell Stock - N.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int current_profit=0;
+        int mini=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+             mini=min(mini,prices[i]);
+            current_profit=max(current_profit,prices[i]-mini);
+           
+        }
+        return current_profit;
+        
+    }
+};
